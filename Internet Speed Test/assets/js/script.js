91407df0c973b5044466631827d0e35526e7b8eb
Rezamns <57560653+Rezamns@users.noreply.github.com>
2024-05-03 05:23:23 +0330

    Add files via upload

---
+++ Internet Speed Test/assets/js/script.js
@@ -0,0 +1,45 @@
+// internet speed test
+var speedButton = document.getElementById('speed-check');
+speedButton.addEventListener('click', InitiateSpeedDetection, false);
+
+var imageAddr = "https://hackthestuff.com/images/test.jpg";
+var downloadSize = 13055440;
+
+function ShowProgressMessage(msg) {
+    document.getElementById('result').innerHTML = msg;
+}
+
+function InitiateSpeedDetection() {
+    ShowProgressMessage("Checking speed, please wait...");
+    window.setTimeout(MeasureConnectionSpeed, 0);
+};
+
+function MeasureConnectionSpeed() {
+    var startTime, endTime;
+    var download = new Image();
+    download.onload = function () {
+        endTime = (new Date()).getTime();
+        showResults();
+    }
+    download.onerror = function (err, msg) {
+        ShowProgressMessage("Error");
+    }
+    startTime = (new Date()).getTime();
+    var cacheBuster = "?nnn=" + startTime;
+    download.src = imageAddr + cacheBuster;
+    function showResults() {
+        var duration = (endTime - startTime) / 1000;
+        var bitsLoaded = downloadSize * 8;
+        var speedBps = (bitsLoaded / duration).toFixed(2);
+        var speedKbps = (speedBps / 1024).toFixed(2);
+        var speedMbps = (speedKbps / 1024).toFixed(2);
+
+        if (speedMbps > 1) {
+            ShowProgressMessage("Your connection speed is " + speedMbps + " Mbps");
+        } else if (speedKbps > 1) {
+            ShowProgressMessage("Your connection speed is " + speedKbps + " kbps");
+        } else {
+            ShowProgressMessage("Your connection speed is " + speedBps + " bps");
+        }
+    }
+}
